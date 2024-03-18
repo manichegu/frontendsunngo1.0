5c84761c74006bb1cfbe3b5c1e3f343886c8ba31
manichegu <92416850+manichegu@users.noreply.github.com>
2024-03-18 22:39:40 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1280,7 +1280,7 @@ Promoting scientific knowledge and curiosity among students through science fair
             <h4 className='mb-4'>FIND US ON</h4>
             <a
                     className="text"
-                    href="mailto:xyz@gmail.com"
+                    href="mailto:sunforindia@gmail.com"
                     style={{ textDecoration: "none", color: "white" }}
                 >
                     <img 
@@ -1294,7 +1294,7 @@ Promoting scientific knowledge and curiosity among students through science fair
     <br />
     <a
       className="text"
-      href="https://www.facebook.com/profile.php?id=100087419500577"
+      href="https://www.facebook.com/profile.php?id=100067494638158&paipv=0&eav=AfYHdBsP-bJtXF-vCoRgWmRRagR32W22PoQbXQTfhxQvOyk7eIn3paRtCaMYLETnDVA"
       style={{ textDecoration: "none", color: "white" }}
     >
       <img
@@ -1308,7 +1308,7 @@ Promoting scientific knowledge and curiosity among students through science fair
     <br />
     <a
       className="text"
-      href="https://www.instagram.com/ajayfromayejude/"
+      href="https://www.instagram.com/student_union_for_nation?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
       style={{ textDecoration: "none", color: "white" }}
     >
       <img
@@ -1321,7 +1321,7 @@ Promoting scientific knowledge and curiosity among students through science fair
     <br />
     <a
       className="text"
-      href="https://www.youtube.com/ayejude"
+      href="https://youtube.com/@studentunionfornationsun639?feature=shared"
       style={{ textDecoration: "none", color: "white" }}
     >
       <img
